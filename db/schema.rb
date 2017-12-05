e0c27bd64e913eea000bec2aa0018a9beade1898
carterfiber <gerald@carterfiber.com>
2017-12-05 16:52:07 -0500

    added List for trello like columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171205214207) do
+ActiveRecord::Schema.define(version: 20171205215057) do
+
+  create_table "lists", force: :cascade do |t|
+    t.string "name"
+    t.integer "position"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
